2fa0fa6db62ce1cc82086ace8ff20acf96d50a7a
Rudra Chavda <rudrachavda@Rudras-Mac.lan>
2023-09-19 21:26:59 -0700

    new xcode file

---
+++ Project Beta/Project Beta/Project_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_BetaApp.swift
+//  Project Beta
+//
+//  Created by Rudra Chavda on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
