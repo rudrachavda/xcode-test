c8e6e861a2bd7023adca0048606399d36610cfd6
Rudra Chavda <rudrachavda@Rudras-Mac.lan>
2023-09-19 23:05:51 -0700

    new changes and new learning experience for xcode, created some basic interaface and tried new items

--- Project Beta/Project Beta/ContentView.swift
+++ Project Beta/Project Beta/ContentView.swift
@@ -1,21 +1,72 @@
-//
-//  ContentView.swift
-//  Project Beta
-//
-//  Created by Rudra Chavda on 9/19/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    @State private var isChecked = false
+    @State private var isDarkMode = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "cloud")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                
+                Text("Rudra Chavda!")
+                
+                Text("Count: \(counter)")
+                
+                Button(action: {
+                    self.counter += 1
+                }) {
+                    Image(systemName: "plus")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+                
+                NavigationLink(destination: AnotherView()) {
+                    Text("Go to Another Page")
+                }
+                
+                TextField("Enter text here", text: .constant(""))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Toggle(isOn: $isChecked) {
+                    Text("Toggle to check")
+                }
+                .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Text("Dark Mode")
+                    Spacer()
+                    Toggle(isOn: $isDarkMode) {
+                    // Empty, toggle will handle it
+                    }
+                    .padding()
+                }
+            }
+            .padding()
+            .navigationTitle("Main Page")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    // Add your action for the top right button here
+                }) {
+                    Image(systemName: "gear")
+                        .imageScale(.large)
+                }
+            )
+            .environment(\.colorScheme, isDarkMode ? .dark : .light)
         }
-        .padding()
+    }
+}
+
+
+struct AnotherView: View {
+    var body: some View {
+        Text("This is another page!")
     }
 }
 
